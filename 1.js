6e69ec26974fc3ad0a41c1ee5a2536bb7fcb6dce
Bobur07K <boburtadaxi@gmail.com>
2021-08-09 11:19:34 +0500

    first commit

---
+++ 1.js
@@ -0,0 +1,36 @@
+// let human = {
+//     name: 'name: MuhammadBobur',
+//     age:  16,
+//     address: ' address Yangi bozor, Andijon'
+// };
+
+// human.age = 17;
+
+
+// console.log('his ' + human.age);
+// let name = prompt('Name'),
+//     age = Number (prompt('Age')),
+//     job = prompt('Your profeccion'),
+//     user = {
+//         ism: name,
+//         yosh: age,
+//         kasb: job,
+//     };
+//     console.log(user);
+// let a = 13>20;
+// console.log(a);
+
+// let aa = [20, 20, 19, 128, 223, '223'],
+//     oo = 20;
+// if(Array.isArray(oo)){
+// console.log('Its Arrray!');
+// } else {
+//     console.log('Its not Array!');
+// }
+let aa = [5, 'laptop', 'sd', 6789, 986,]
+    num = aa[2];
+    if(typeof(num) == 'number'){ 
+        console.log('It first element');
+    } else {
+        console.log('Its second element');
+    }
